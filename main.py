1c1d768c52876d9cc73e1320ee9d5b0c876154c2
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2020-05-27 13:17:41 +0300

    download books with titles

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import requests
+from bs4 import BeautifulSoup
 import os
+from pathvalidate import sanitize_filename
 
-if not os.path.exists("Books"):
-    os.makedirs("Books")
+def download_txt(url, filename, folder='books/'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    sanitized_filename = sanitize_filename(filename)
+    if response.status_code == 200:
+        with open("{}/{}.txt".format(folder, sanitized_filename), "w") as my_file:
+            my_file.write(response.text)
+
 
 for book_id in range(1, 11):
-    book_url = 'http://tululu.org/txt.php?id={}'.format(book_id)
-    response = requests.get(book_url, allow_redirects=False)
+    site_url = 'http://tululu.org/b{}/'.format(book_id)
+    response = requests.get(site_url, allow_redirects=False)
     response.raise_for_status()
     if response.status_code == 200:
-        with open("Books/id{}.txt".format(book_id), "w") as my_file:
-            my_file.write(response.text)
+        soup = BeautifulSoup(response.text, 'lxml')
+        title_tag = soup.find('div', id='content').find('h1')
+        title_and_author = title_tag.text.split("   ::   ")
+        title = "{}. {}".format(book_id, title_and_author[0])
+        author = title_and_author[1]
+    book_url = 'http://tululu.org/txt.php?id={}'.format(book_id)
+    download_txt(book_url, title)
+
+
+
