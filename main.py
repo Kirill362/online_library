439bb4fc82e6ab2632fc2ba841cb11644bf30bd6
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2020-05-28 09:32:46 +0300

    download book covers.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin
 
 def download_txt(url, filename, folder='books/'):
     if not os.path.exists(folder):
@@ -12,7 +13,16 @@ def download_txt(url, filename, folder='books/'):
     if response.status_code == 200:
         with open("{}/{}.txt".format(folder, sanitized_filename), "w") as my_file:
             my_file.write(response.text)
+    return os.path.join('folder', 'filename')
 
+def download_image(url, filename, folder='images/'):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    response = requests.get(url, allow_redirects=False)
+    response.raise_for_status()
+    with open("{}/{}".format(folder, filename), "wb") as my_file:
+        my_file.write(response.content)
+    return os.path.join('folder', 'filename')
 
 for book_id in range(1, 11):
     site_url = 'http://tululu.org/b{}/'.format(book_id)
@@ -24,8 +34,13 @@ for book_id in range(1, 11):
         title_and_author = title_tag.text.split("   ::   ")
         title = "{}. {}".format(book_id, title_and_author[0])
         author = title_and_author[1]
-    book_url = 'http://tululu.org/txt.php?id={}'.format(book_id)
-    download_txt(book_url, title)
+        picture_tag = soup.find('div', class_="bookimage").find('a').find('img')['src']
+        full_image_url = urljoin(site_url, picture_tag)
+        filename = full_image_url.split("/")[-1]
+    download_image(full_image_url, filename)
+    #book_url = 'http://tululu.org/txt.php?id={}'.format(book_id)
+    #download_txt(book_url, title)
+
 
 
 
