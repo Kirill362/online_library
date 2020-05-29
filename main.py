b67e607fa4d1d9b2eba505a8e1c7b5802a0d97af
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2020-05-29 13:12:23 +0300

    parse books genres.

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def download_image(url, filename, folder='images/'):
         my_file.write(response.content)
     return os.path.join('folder', 'filename')
 
+genres = []
 for book_id in range(1, 11):
     site_url = 'http://tululu.org/b{}/'.format(book_id)
     response = requests.get(site_url, allow_redirects=False)
@@ -40,11 +41,17 @@ for book_id in range(1, 11):
         full_image_url = urljoin(site_url, picture_tag)
         filename = full_image_url.split("/")[-1]
         comment_tag = soup.find_all('div', class_='texts')
-        if comment_tag == []:
-            print("Комментариев пока нет")
-        else:
-            for comment in comment_tag:
-                print(comment.find('span').text)
+        #if comment_tag == []:
+            #print("Комментариев пока нет")
+        #else:
+            #for comment in comment_tag:
+                #print(comment.find('span').text)
+
+        genre_tag = soup.find('span', class_='d_book').find_all('a')
+        for tag in genre_tag:
+            genres.append(tag.text)
+        print(genres)
+        genres = []
     #download_image(full_image_url, filename)
     #book_url = 'http://tululu.org/txt.php?id={}'.format(book_id)
     #download_txt(book_url, title)
