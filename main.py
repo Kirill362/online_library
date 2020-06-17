764cca97fe1c71279257ab7197a50cfeec203a7f
Kirill362 <64096313+Kirill362@users.noreply.github.com>
2020-06-17 19:45:09 +0300

    Исправил ошибки

--- main.py
+++ main.py
@@ -28,12 +28,12 @@ def download_file(url, path, text=True):
 
 def create_book_information(code, link):
     soup = BeautifulSoup(code, 'lxml')
-    book_information = find_title(soup)
-    book_information.update({"author": find_author(soup),
+    book_card = find_title(soup)
+    book_card.update({"author": find_author(soup),
                              "img_src": find_img(soup, link),
                              "comments": [comment.find('span').text for comment in soup.select('div.texts')],
                              "genres": [tag.text for tag in soup.select('span.d_book a')]})
-    return book_information
+    return book_card
 
 
 def find_title(soup):
@@ -77,7 +77,7 @@ def check_redirect(response):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Скачивание книг с определённых страниц')
     parser.add_argument('--start_page', help='С какой страницы начать скачивание', default=1, type=int)
-    parser.add_argument('--end_page', help='На какой странице закончить скачивание', default=5, type=int)
+    parser.add_argument('--end_page', help='На какой странице закончить скачивание', default=find_last_page(), type=int)
     parser.add_argument('--dest_folder', help='Путь к каталогу с результатами парсинга: картинкам, книгами, JSON', default='./')
     parser.add_argument('--skip_imgs', help='Hе скачивать картинки', default=False, action='store_true')
     parser.add_argument('--skip_txt', help='Hе скачивать книги', default=False, action='store_true')
@@ -109,32 +109,35 @@ if __name__ == '__main__':
                 continue
             for attempt in range(1, 6):
                 try:
-                    book_id = book_url[2:-1]
-                    book_information = create_book_information(response.text, full_book_url)
+                    book_id = book_url.split("/")[-2][1:]
+                    book_card = create_book_information(response.text, full_book_url)
                     if not args.skip_imgs:
-                        img_id = book_information['img_src'].split('/')[-1]
+                        img_id = book_card['img_src'].split('/')[-1]
                         filename = f"{book_id}. {img_id}"
                         folder = os.path.join(args.dest_folder, "images")
                         os.makedirs(folder, exist_ok=True)
                         path = os.path.join(folder, filename)
-                        download_file(book_information["img_src"], path, False)
+                        download_file(book_card["img_src"], path, False)
                     download_book_url = 'http://tululu.org/txt.php?id={}'.format(book_url[2:-1])
                     if not args.skip_txt:
-                        filename = f"{book_id}. {book_information['title']}"
+                        filename = f"{book_id}. {book_card['title']}"
                         sanitized_filename = sanitize_filename(filename)
                         folder = os.path.join(args.dest_folder, "books")
                         os.makedirs(folder, exist_ok=True)
                         path = os.path.join(folder, f'{sanitized_filename}.txt')
-                        book_information["book_path"] = path
+                        book_card["book_path"] = path
                         download_file(download_book_url, path)
-                    books_list.append(book_information)
+                    books_list.append(book_card)
                     break
-                except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError) as error:
+                except requests.exceptions.ConnectionError as error:
                     logging.warning(f"Попытка подключения №{attempt}")
                     if attempt == 5:
                         logging.error("Превышено количество попыток подключения, проверьте сетевое подключение")
                         sys.exit()
                     time.sleep(5)
+                except requests.exceptions.HTTPError as error:
+                    logging.error("Неправильный запрос или поломки на сервере")
+                    break
 
     os.makedirs("{}{}".format(args.dest_folder, args.json_path), exist_ok=True)
 
